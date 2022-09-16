f8a40ed485a96e9a3bb51c73fccd7d81f86aa046
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2022-09-16 23:13:38 +0200

    💄 deskStructure: Set singleton document titles

--- sanity/deskStructure.js
+++ sanity/deskStructure.js
@@ -6,20 +6,28 @@ export default () =>
     .items([
       S.listItem()
         .title('Bio')
-        .child(S.document().schemaType('bio').documentId('bio')),
+        .child(S.document().schemaType('bio').documentId('bio').title('Bio')),
       S.listItem()
         .title('Banner Message')
         .child(
-          S.document().schemaType('bannerMessage').documentId('bannerMessage')
+          S.document()
+            .schemaType('bannerMessage')
+            .documentId('bannerMessage')
+            .title('Banner Message')
         ),
       S.listItem()
         .title('Lyrics Marquee')
         .child(
-          S.document().schemaType('lyricsMarquee').documentId('lyricsMarquee')
+          S.document()
+            .schemaType('lyricsMarquee')
+            .documentId('lyricsMarquee')
+            .title('Lyrics Marquee')
         ),
       S.listItem()
         .title('About')
-        .child(S.document().schemaType('about').documentId('about')),
+        .child(
+          S.document().schemaType('about').documentId('about').title('About')
+        ),
       ...S.documentTypeListItems().filter(
         (item) =>
           !['bio', 'bannerMessage', 'lyricsMarquee', 'about'].includes(
